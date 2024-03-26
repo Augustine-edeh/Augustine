f785b5ff8eee479a0262d4e0cf76a125a7aff35b
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-03-26 18:26:44 +0100

    improve semantic tags mobile nav

--- src/components/navigation/BottomNav.tsx
+++ src/components/navigation/BottomNav.tsx
@@ -4,24 +4,18 @@ const BottomNav = () => {
   return (
     <nav className="fixed md:hidden bottom-5 right-0 left-0 bg-slate-800 backdrop-blur-[6px] shadow-xl rounded-full w-[90dvw] mx-auto text-white">
       <ul className="flex justify-evenly py-2">
-        <li>
-          <a href="" className="grid place-items-center">
-            <Icons.UserCircleIcon className="h-8" />
-            <span>About</span>
-          </a>
-        </li>
-        <li className="grid place-items-center">
-          <a href="" className="grid place-items-center">
-            <Icons.BriefcaseIcon className="h-8" />
-            <span>Projects</span>
-          </a>
-        </li>
-        <li className="grid place-items-center">
-          <a href="" className="grid place-items-center">
-            <Icons.EnvelopeIcon className="h-8" />
-            <span>Contact</span>
-          </a>
-        </li>
+        <a href="#about" className="grid place-items-center">
+          <Icons.UserCircleIcon id="about" className="h-8" />
+          <label htmlFor="about">About</label>
+        </a>
+        <a href="#projects" className="grid place-items-center">
+          <Icons.BriefcaseIcon id="projects" className="h-8" />
+          <label htmlFor="projects">Projects</label>
+        </a>
+        <a href="#contact" className="grid place-items-center">
+          <Icons.EnvelopeIcon id="contact" className="h-8" />
+          <label htmlFor="contact">Contact</label>
+        </a>
       </ul>
     </nav>
   );
