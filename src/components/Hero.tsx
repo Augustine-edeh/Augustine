1d92257fa765e576e84598ce04a6119b69323c0a
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-03-26 18:02:20 +0100

    move image container to top

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,22 +2,36 @@ import profileImg from "../assets/images/profileImg.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faColonSign } from "@fortawesome/free-solid-svg-icons";
 
 const Hero = () => {
   return (
     // FIXME: height should = screen - 20 tailwind units
     // FIXME: 20 units is from h-20 on header component
     <div className="min-h-[calc(100vh-5rem)] flex flex-col gap-20 lg:flex-row  justify-center items-center bg-slate-900">
+      <section className="flex h-56">
+        <div className="container h-full w-56 bg-black rounded-lg overflow-clip z-10 shadow-2xl">
+          <img
+            // src={"https://xsgames.co/randomusers/assets/avatars/male/74.jpg"}
+            src={profileImg}
+            alt="Augustine"
+            className="h"
+          />
+        </div>
+        <div className="h-full w-56 h bg-slate-800 rounded-lg -ml-44 mt-8 shadow-xl"></div>
+      </section>
+
       <section className="text-white text-center">
-        <h3 className="text-2xl">Hello 👋, I'm</h3>
+        <h3 className="text-2xl">Hello, I'm</h3>
         <div>
-          <p className="text-3xl"> Augustine Edeh</p>
-          <p className="p-0 mt-8 text-xl">
+          <p className="text-3xl my-5 font-extrabold"> Augustine Edeh</p>
+          <p className="p-0 mt-8">
             an{" "}
-            <span className="text-teal-500">Advancing frontend developer</span>{" "}
-            & <span className="text-teal-500">a fullstack developer</span> in
-            the making (self tught)
+            <span className="text-teal-500 text-2xl">
+              Advancing frontend developer
+            </span>{" "}
+            &{" "}
+            <span className="text-blue-400 text-lg">a fullstack developer</span>{" "}
+            in the making (self tught)
           </p>
         </div>
 
@@ -55,19 +69,6 @@ const Hero = () => {
             />
           </button>
         </div>
-      </section>
-
-      <section className="flex place-content-center h-56 bg-blue-40 mb-20">
-        {/* FIXME: remeber to remove mb-20 */}
-        <div className="container h-full w-56 bg-black rounded-lg overflow-clip z-10 shadow-lg">
-          <img
-            // src={"https://xsgames.co/randomusers/assets/avatars/male/74.jpg"}
-            src={profileImg}
-            alt="Augustine"
-            className="h"
-          />
-        </div>
-        <div className="h-full w-56 h bg-slate-800 rounded-lg -ml-44 mt-8"></div>
       </section>
     </div>
   );
