2f05eb1af8985798dac52898c519dfde0ec2fedf
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-04-22 17:18:57 +0100

    fea(Subtle bottom-left shadow effect): - This commit adds a subtle background shadow effect on the bottom left corner (only for md: breakpoint & above)

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,14 @@ import Projects from "./pages/Projects";
 BottomNav;
 const App = () => {
   return (
-    <main className="bg-slate-900">
+    <main className="bg-slate-900 relative overflow-hidden">
       <Header />
       <Hero />
       <About />
       <Projects />
       <Contact />
       <BottomNav />
+      <div className="hidden md:block fixed h-40 w-40 shadow-[50px_-80px_250px_#419aff93] bg-slate-700/[11%] bottom-0 left-0 rounded-full -ml-36 -mb-32"></div>
     </main>
   );
 };
