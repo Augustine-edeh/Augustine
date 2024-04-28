100a74aebfd1f4ced3e722a852d2705aa7c757fd
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-04-28 23:43:40 +0100

    feat(Projects): add stable project items styles

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -6,7 +6,7 @@ const Projects = () => {
       title: "Countries at a glance",
       description:
         "A dynamic level react project, utuilizing the restCountries API for getting countries inforainformations and other general knowledge about countries",
-      image: "https://i.imgur.com/6sCnlAq.png",
+      image: "/vite.svg",
       githubUrl: "https://github.com/augustine-edeh/countries-at-a-glance",
       liveUrl: "https://countriesataglance.vercel.app",
       technologiesUSed: [
@@ -21,7 +21,7 @@ const Projects = () => {
       title: "Qr Xpress",
       description:
         "A QR code hub for creating, downloading and scanning  qr codes",
-      image: "https://i.imgur.com/6sCnlAq.png",
+      image: "/vite.svg",
       githubUrl: "https://github.com/augustine-edeh/Qr-Xpress",
       liveUrl: "https://qr-xpress.vercel.app",
       technologiesUSed: ["Vite", "React.js", "TailwindCSS", "Vercel"],
@@ -30,7 +30,7 @@ const Projects = () => {
       title: "Damilare",
       description:
         "A complete functional portfolio webpage I creared for my 7 years old nephew who is developing an interest in technology and games",
-      image: "https://i.imgur.com/6sCnlAq.png",
+      image: "/vite.svg",
       githubUrl: "https://github.com/augustine-edeh/damilare",
       liveUrl: "https://damilareai.vercel.app",
       technologiesUSed: ["Vite", "React.js", "TailwindCSS", "Vercel"],
@@ -39,7 +39,7 @@ const Projects = () => {
       title: "Dynamic Calculator web app",
       description:
         "Multi-arithmetic calculator web app. For non-scientific arithmatic calculations, with a subtle error vibration indiccator function for mobile devices",
-      image: "https://i.imgur.com/6sCnlAq.png",
+      image: "/vite.svg",
       githubUrl: "https://github.com/augustine-edeh/dynamic-calculator",
       liveUrl: "https://dynamic-calculator.vercel.app",
       technologiesUSed: ["Vite", "React.js", "TailwindCSS", "Vercel"],
@@ -48,7 +48,7 @@ const Projects = () => {
       title: "Gym Buddy",
       description:
         "A MERN stack web application for tracking workout exercises",
-      image: "https://i.imgur.com/6sCnlAq.png",
+      image: "/vite.svg",
       githubUrl: "https://github.com/augustine-edeh/Gym-Buddy",
       liveUrl: "https://gym-buddy.vercel.app",
       technologiesUSed: [
@@ -65,7 +65,7 @@ const Projects = () => {
       title: "Go Country",
       description:
         "Former Version of the Countries at a glance project. Get general information on the countries of the world",
-      image: "https://i.imgur.com/6sCnlAq.png",
+      image: "/vite.svg",
       githubUrl: "https://github.com/augustine-edeh/Gocountry.vercel.app",
       liveUrl: "https://Gocountry.vercel.app",
       technologiesUSed: [
@@ -81,7 +81,7 @@ const Projects = () => {
   return (
     <section
       id="projects"
-      className="min-h-screen max-w-screen-md px-8 mx-auto text-white pt-20 bg-red-30"
+      className="min-h-screen max-w-screen-lg px-8 mx-auto text-white pt-20 bg-red-30"
     >
       <p className="font-bold text-gray-400">My works?</p>
 
@@ -91,14 +91,18 @@ const Projects = () => {
 
       <p className="text-gray-400">Checkout some of my works...</p>
 
-      <ul className="flex flex-wrap justify-center gap-x-3 gap-y-10 mt-20 p-2">
+      {/* project items */}
+      <ul className="flex flex-wrap justify-center gap-x-3 gap-y-28 lg:gap-y-60 mt-20 p-">
         {projectList &&
           projectList.map((project) => (
             <li
               key={Math.random()}
-              className="flex flex-col w-96 rounded-2xl overflow-hidden ring-1  p-4"
+              className="flex flex-col rounded-2xl lg:rounded-none overflow-hidden ring- shadow-2xl max-w-96 h-[calc(100dvh-200px)] 
+              lg:h-[calc(100dvh/2)] lg:flex-row lg:max-w-screen-xl lg:shadow-none"
+              // project items height is in the above logic (min-height)
             >
-              <div className="bg-blue-300 h-[48%]">
+              {/* project image section */}
+              <div className="bg-blue-300 h-[40%] lg:w-1/2 lg:h-full lg:w-[80%]">
                 <img
                   src={project.image}
                   alt={project.title}
@@ -106,23 +110,25 @@ const Projects = () => {
                 />
               </div>
 
-              <div className="relative flex flex-col mx-4 h-[52%] py-3">
-                <h4 className="text-center text-lg font-bold uppercase mb-4">
+              {/* text area section */}
+              <div className="flex flex-col h-[60%] lg:h-auto py-3 px-3 bg-slate-800/80  text-center text-sm lg:w-1/2 lg:-ml-10 lg:mt-16 lg:mb-2 lg:shadow-xl ">
+                <h4 className="text-xl font-bold uppercase mt-3 lg:mt-10">
                   {project.title}
                 </h4>
 
-                <p className="">{project.description}</p>
+                <p className="my-7">{project.description}</p>
 
-                <ul className="flex flex-wrap py-5 gap-x-2 gap-y-2">
+                {/* technologies used */}
+                <ul className="flex flex-wrap gap-x-2 gap-y-2 justify-center">
                   {project.technologiesUSed.map((technology) => (
-                    <li className="bg-gray-700 px-2 py-0.5 rounded">
+                    <li className="bg-gray-700/50 px-2 py-0.5 rounded">
                       <span>{technology}</span>
                     </li>
                   ))}
                 </ul>
 
-                <div className="flex justify-center gap-x-5 text-center absolute bottom-0 w-full pb-5">
-                  <button className="  bg-blue-400 rounded-2xl px-4 py-1.5">
+                <div className="flex justify-center  gap-x-5 mt-auto">
+                  <button className="  bg-blue-700 rounded-2xl px-4 py-1.5">
                     <a
                       href={project.liveUrl}
                       target="_blank"
