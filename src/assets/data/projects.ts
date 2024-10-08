044f79319717b774dca77875384a0b1b194c5153
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-10-08 11:07:31 +0100

    chore: rename projectList.js to projectList.ts
    - Converted projectList.js to TypeScript for better type safety
    - Updated imports and adjusted code where necessary for TypeScript compatibility

---
+++ src/assets/data/projects.ts
@@ -0,0 +1,93 @@
+const projectList = [
+  {
+    title: "Countries at a Glance",
+    description:
+      "A dynamic React project utilizing the Rest Countries API to retrieve and display general information about countries worldwide.",
+    cldImg_publicId: "portfolio_project/Countries-at-a-glance_laptopView",
+    githubUrl: "https://github.com/augustine-edeh/countries-at-a-glance",
+    liveUrl: "https://countries-at-a-glance.vercel.app",
+    technologiesUsed: [
+      "Vite",
+      "React.js",
+      "TailwindCSS",
+      "Rest Countries API",
+      "Vercel",
+    ],
+  },
+  {
+    title: "My Todoly",
+    description:
+      "A task management app built with Next.js, featuring task addition, deletion, and filtering. The app includes a drag-and-drop reordering feature available on large screens and above.",
+    cldImg_publicId: "portfolio_project/Todoly_laptopView",
+    githubUrl: "https://github.com/Augustine-edeh/Todoly",
+    liveUrl: "https://mytodoly.vercel.app/",
+    technologiesUsed: [
+      "Next.js",
+      "React.js",
+      "TailwindCSS",
+      "Zustand",
+      "Vercel",
+    ],
+  },
+  {
+    title: "Qr Xpress",
+    description:
+      "A QR code hub for creating, downloading, and scanning QR codes, built with a simple and responsive interface.",
+    cldImg_publicId: "portfolio_project/Qr-Xpress_laptopView",
+    githubUrl: "https://github.com/Augustine-edeh/QrXpress",
+    liveUrl: "https://qr-xpress.vercel.app",
+    technologiesUsed: ["Vite", "React.js", "TailwindCSS", "Vercel"],
+  },
+  {
+    title: "Damilare",
+    description:
+      "A functional portfolio website created for my 7-year-old nephew, who is developing an interest in technology and gaming.",
+    cldImg_publicId: "portfolio_project/Damilare_laptopView",
+    githubUrl: "https://github.com/augustine-edeh/damilare",
+    liveUrl: "https://damilareai.vercel.app",
+    technologiesUsed: ["Vite", "React.js", "TailwindCSS", "Vercel"],
+  },
+  {
+    title: "Dynamic Calculator Web App",
+    description:
+      "A multi-arithmetic calculator for non-scientific calculations, featuring a subtle error vibration indicator function for mobile devices.",
+    cldImg_publicId: "portfolio_project/Dynamic-calculator_laptopView",
+    githubUrl: "https://github.com/Augustine-edeh/Dynamic-Calculator-Web-app",
+    liveUrl: "https://augustine-edeh.github.io/Dynamic-Calculator-Web-app",
+    technologiesUsed: ["Vite", "React.js", "TailwindCSS", "Vercel"],
+  },
+  {
+    title: "Gym Buddy",
+    description:
+      "A MERN stack application for tracking workout exercises, with a focus on fitness enthusiasts.",
+    cldImg_publicId: "portfolio_project/Gym-buddy_laptopView", // Update this URL with the actual image link
+    githubUrl: "https://github.com/augustine-edeh/Gym-Buddy",
+    liveUrl: "https://gym-buddy.vercel.app",
+    technologiesUsed: [
+      "Vite",
+      "Typescript",
+      "React.js",
+      "TailwindCSS",
+      "MongoDB",
+      "ExpressJS",
+      "Vercel",
+    ],
+  },
+  {
+    title: "Go Country",
+    description:
+      "An earlier version of the 'Countries at a Glance' project. It provides general information on the countries of the world using the Rest Countries API.",
+    cldImg_publicId: "portfolio_project/Go-country_laptopView",
+    githubUrl: "https://github.com/Augustine-edeh/GoCountry",
+    liveUrl: "https://augustine-edeh.github.io/GoCountry",
+    technologiesUsed: [
+      "Vite",
+      "React.js",
+      "TailwindCSS",
+      "Rest Countries API",
+      "Vercel",
+    ],
+  },
+];
+
+export default projectList;
