944a008ce33f061f593975277cda1b4ba9bd1e63
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-10-20 11:00:41 +0100

    fix: fix layout glitch for `Projects` section

--- src/components/ui/ProjectCard.tsx
+++ src/components/ui/ProjectCard.tsx
@@ -13,13 +13,13 @@ const ProjectCard = ({ project, index }) => {
   return (
     <Tilt key={project.githubUrl}>
       <li
-        className={`flex flex-col rounded-2xl lg:rounded-none overflow-hidden ring-2 lg:ring-0 shadow-2xl max-w-96 h-[calc(100vh-250px)] 
-lg:h-[calc(100vh/2.1)] lg:max-w-screen-xl lg:shadow-none ${
+        className={`flex flex-col rounded-2xl bg-red-500 lg:rounded-none overflow-hidden ring-2 lg:ring-0 shadow-2xl max-w-96 h-[calc(100vh-250px)] 
+lg:h-[calc(100vh/2.1)] lg:max-w-7xl lg:shadow-none ${
           index % 2 === 0 ? "lg:flex-row" : "lg:flex-row-reverse"
         }`}
       >
-        {/* project image section */}
-        <div className="bg-blue-300 h-[40%] lg:h-full lg:w-[80%] m-2 rounded-md overflow-hidden">
+        {/* Project image section */}
+        <div className="bg-blue-300 h-[40%] lg:h-full lg:w-[90%] m-2 lg:m-0 rounded-md overflow-hidden">
           <AdvancedImage
             cldImg={cld.image(`${project.cldImg_publicId}`)}
             className="h-full w-full"
@@ -27,29 +27,35 @@ lg:h-[calc(100vh/2.1)] lg:max-w-screen-xl lg:shadow-none ${
           />
         </div>
 
-        {/* text area section */}
+        {/* Project text section */}
+        {/* FIXME: remove dead text-sm style class below */}
         <div
-          className={`z-10 flex flex-col h-[60%] lg:h-auto py-3 px-3 bg-slate-800/80 text-center text-sm lg:w-1/2 ${
+          className={`z-10 flex flex-col h-[60%] lg:h-auto py-3 px-3 bg-slate-800/80 text-center text- sm  lg:w-1/2 lg :w-[50%] ${
             index % 2 === 0 ? "lg:-ml-10" : "lg:-mr-10"
-          } lg:mt-16 lg:mb-2 lg:shadow-xl`}
+          } lg:mt-10 lg:mb-2 lg:shadow-xl overflow-scroll`}
         >
-          <h4 className="text-2xl font-bold uppercase mt-3 lg:mt-10">
+          {/* Project title */}
+          <h4 className="text-2xl font-bold uppercase mt-3 lg:mt-4">
             {project.title}
           </h4>
 
-          <p className="my-7 text-[14px] text-gray-300">
+          <hr />
+
+          {/* Project description */}
+          <p className="my-7 text-sm lg:text-lg text-gray-300">
             {project.description}
           </p>
 
-          {/* technologies used */}
+          {/* Technologies used */}
           <ul className="flex flex-wrap gap-2 justify-center">
             {project.technologiesUsed.map((technology, index) => (
               <li key={index} className="bg-gray-700/50 px-2 py-0.5 rounded">
-                <span>{technology}</span>
+                {technology}
               </li>
             ))}
           </ul>
 
+          {/* Project links */}
           <div className="flex justify-center gap-x-5 mt-auto">
             <a
               href={project.liveUrl}
