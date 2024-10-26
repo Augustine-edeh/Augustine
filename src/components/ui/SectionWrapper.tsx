1ac5f4bf64203555ae343ef74af2ad85a539f469
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-10-26 21:19:18 +0100

    chore(section wrapper): reset `SectionWrapper` background to 'transparent'

--- src/components/ui/SectionWrapper.tsx
+++ src/components/ui/SectionWrapper.tsx
@@ -2,7 +2,7 @@ const SectionContainer = ({ children, id }) => {
   return (
     <section
       id={id}
-      className={`min-h-screen max-w-screen-lg px-8 pt-32 mx-auto bg-[#F9FAFB] dark:bg-slate-900`}
+      className={`min-h-screen max-w-screen-lg px-8 pt-32 mx-auto bg-transparent`}
 
       //   contact
       //   className="min-h-screen max-w-screen-lg px-8 py-32 mx-auto text-white"
