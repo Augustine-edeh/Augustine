cfac151d66adfad9b41be7d6189fe6fff968be45
Augustine Edeh <info.augustinesedeh@gmail.com>
2025-04-08 23:53:59 +0100

    feat: add Lenis for smooth scrolling experience

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 import Header from "./components/ui/Header";
 import Hero from "./pages/Hero";
 import BottomNav from "./components/navigation/BottomNav";
@@ -6,7 +8,20 @@ import About from "./pages/About";
 import Projects from "./pages/Projects";
 import LightUI from "./components/ui/LightUI";
 
+import Lenis from "lenis";
+
 const App = () => {
+  // Lenis Logic
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time: any) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <main className="bg-[#F9FAFB] dark:bg-slate-900 relative overflow-hidden">
       <Header />
